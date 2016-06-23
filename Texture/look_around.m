378300d480ca168088d24121965df3ca5ba84676
fdcb <fdcb@live.com.pt>
2016-06-23 18:12:32 +0100

    Testing

--- Texture/look_around.m
+++ Texture/look_around.m
@@ -1,8 +1,13 @@
 function [ cordX, cordY ] = look_around(x, y, im, spot)
 	cordX = x;
 	cordY = y;
+	
+	color = im(y, x, :);
+	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+		return;
+	end
+	
 	color = im((y + 1), x, :);
-
 	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
 		return;
 	end
@@ -44,26 +49,30 @@ function [ cordX, cordY ] = look_around(x, y, im, spot)
 
 	switch spot
 		case 'l'
-			color = im(y, (x + 2), :);
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+				cordX = (x + 2);
+				cordY = y;
 				return;
 			end
 			[cordX, cordY] = look_around((x + 2), y, im, spot);
 		case 'r'
-			color = im(y, (x + 2), :);
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+				cordX = (x - 2);
+				cordY = y;
 				return;
 			end
-			[cordX, cordY] = look_around((x + 2), y, im, spot);
+			[cordX, cordY] = look_around((x - 2), y, im, spot);
 		case 'u'
-			color = im((y + 2), x, :);
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+				cordX = x;
+				cordY = (y + 2);
 				return;
 			end
 			[cordX, cordY] = look_around(x, (y + 2), im, spot);
 		case 'd'
-			color = im((y - 2), x, :);
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+				cordX = x;
+				cordY = (y - 2);
 				return;
 			end
 			[cordX, cordY] = look_around(x, (y - 2), im, spot);
