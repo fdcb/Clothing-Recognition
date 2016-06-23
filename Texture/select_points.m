56fae2ba95622c3566c112cb1f5a5897c2773269
fdcb <fdcb@live.com.pt>
2016-06-23 17:51:12 +0100

    Testing

--- Texture/select_points.m
+++ Texture/select_points.m
@@ -9,12 +9,8 @@ disp(points)
 		plot(cordX, cordY, 'c+');
 		color = im(cordY, cordX, :);
 		if color(1) == 0 && color(2) == 0 && color(3) == 0
-			disp(11111)
 			[newX, newY] = look_around(cordX, cordY, im, spot);
-			disp(newX)
-			disp(newY)
-			disp(cordX)
-			disp(cordY)
+		plot(cordX, cordY, 'm+');
 			if newX ~= cordX || newY ~= cordY
 				x(index) = cordX;
 				y(index) = cordY;
