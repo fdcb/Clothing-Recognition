d2bfedaaa9bd00962266eccee3268b9717c497d5
fdcb <fdcb@live.com.pt>
2016-06-23 12:20:53 +0100

    Testing

--- Texture/select_points.m
+++ Texture/select_points.m
@@ -1,13 +1,13 @@
 function [ x, y ] = select_points(x, y, im)
 	points = find_outermostPoints(x, y);
 disp(points)
-disp(232323)
 disp(length(points))
 	for i = 1 : length(points)
 		cordX = round(cell2float(points(i, 1)));
 		cordY = round(cell2float(points(i, 2)));
 		spot = char(points(i, 3));
 		index = cell2mat(points(i, 4));
+disp(232323)
 		color = im(cordY, cordX, :);
 		if color(1) == 0 && color(2) == 0 && color(3) == 0
 			disp(11111)
