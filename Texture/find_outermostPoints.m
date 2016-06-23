48d4f215e58a3a46e3716c704a54a2d39cee311d
fdcb <fdcb@live.com.pt>
2016-06-23 19:34:08 +0100

    Find_outermost now finds 2 points for each side

--- Texture/find_outermostPoints.m
+++ Texture/find_outermostPoints.m
@@ -11,4 +11,19 @@ function [ points ] = find_outermostPoints(x, y)
 	points(3, :) = {x(iMaxY), maxY, 'd', iMaxY};
 	points(4, :) = {x(iMinY), minY, 'u', iMinY};
 
+	x(iMaxX) = [];
+	x(iMinX) = [];
+	y(iMaxY) = [];
+	y(iMinY) = [];
+
+	[maxX, iMaxX] = max(x);
+	[minX, iMinX] = min(x);
+	[minY, iMinY] = min(y);
+	[maxY, iMaxY] = max(y);
+
+	points(5, :) = {maxX, y(iMaxX), 'r', iMaxX};
+	points(6, :) = {minX, y(iMinX), 'l', iMinX};
+	points(7, :) = {x(iMaxY), maxY, 'd', iMaxY};
+	points(8, :) = {x(iMinY), minY, 'u', iMinY};
+
 end
