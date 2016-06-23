e04e43bbd6d01f248dc5ec84db726f3d2ab0d6bd
fdcb <fdcb@live.com.pt>
2016-06-23 11:20:06 +0100

    Corrected coding error

--- Texture/look_around.m
+++ Texture/look_around.m
@@ -2,8 +2,7 @@ function [ cordX, cordY ] = look_around(x, y, im, spot)
 	cordX = x;
 	cordY = y;
 	color = im((y + 1), x, :);
-disp(x)
-disp(y)
+
 	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
 		return;
 	end
@@ -49,24 +48,24 @@ disp(y)
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
 				return;
 			end
-			[cordX, cordY] = look_around((y + 2), x, im, spot);
+			[cordX, cordY] = look_around(x, (y + 2), im, spot);
 		case 'r'
 			color = im((y - 2), x, :);
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
 				return;
 			end
-			[cordX, cordY] = look_around((y - 2), x, im, spot);
+			[cordX, cordY] = look_around(x, (y - 2), im, spot);
 		case 'u'
 			color = im(y, (x - 2), :);
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
 				return;
 			end
-			[cordX, cordY] = look_around(y, (x - 2), im, spot);
+			[cordX, cordY] = look_around((x - 2), y, im, spot);
 		case 'd'
 			color = im(y, (x + 2), :);
 			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
 				return;
 			end
-			[cordX, cordY] = look_around(y, (x + 2), im, spot);
+			[cordX, cordY] = look_around((x + 2), y, im, spot);
 	end
 end
