655502c627d026ad80eac54816f74697b945c6ba
fdcb <fdcb@live.com.pt>
2016-06-23 19:24:47 +0100

    Changed "black" maximum values

--- Texture/look_around.m
+++ Texture/look_around.m
@@ -3,74 +3,74 @@ function [ cordX, cordY ] = look_around(x, y, im, spot)
 	cordY = y;
 
 	color = im(y, x, :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 	
 	color = im((y + 1), x, :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	color = im((y + 1), (x + 1), :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	color = im((y - 1), (x + 1), :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	color = im((y - 1), (x - 1), :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	color = im((y + 1), (x - 1), :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	color = im((y - 1), x, :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	color = im(y, (x - 1), :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	color = im(y, (x + 1), :);
-	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+	if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 		return;
 	end
 
 	switch spot
 		case 'l'
-			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+			if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 				cordX = (x + 2);
 				cordY = y;
 				return;
 			end
 			[cordX, cordY] = look_around((x + 2), y, im, spot);
 		case 'r'
-			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+			if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 				cordX = (x - 2);
 				cordY = y;
 				return;
 			end
 			[cordX, cordY] = look_around((x - 2), y, im, spot);
 		case 'u'
-			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+			if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 				cordX = x;
 				cordY = (y + 2);
 				return;
 			end
 			[cordX, cordY] = look_around(x, (y + 2), im, spot);
 		case 'd'
-			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
+			if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 				cordX = x;
 				cordY = (y - 2);
 				return;
