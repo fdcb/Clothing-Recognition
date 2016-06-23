63c3423a9cbaaf90b20c0b0ad9d52d633aeaad1e
fdcb <fdcb@live.com.pt>
2016-06-23 11:50:00 +0100

    Testing

--- Texture/select_points.m
+++ Texture/select_points.m
@@ -12,10 +12,10 @@ function [ x, y ] = select_points(x, y, im)
 		color = im(cordY, cordX, :);
 		if color(1) == 0 && color(2) == 0 && color(3) == 0
 			[newX, newY] = look_around(cordX, cordY, im, spot);
-			newX
-			newY
-			cordX
-			cordY
+			disp(newx)
+			disp(newy)
+			disp(cordx)
+			disp(cordy)
 			if newX ~= cordX || newY ~= cordY
 				x(index) = cordX;
 				y(index) = cordY;
