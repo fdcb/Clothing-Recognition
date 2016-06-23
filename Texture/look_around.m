72e51fc0d395b3024a5a3f46199bee38c3759fbb
fdcb <fdcb@live.com.pt>
2016-06-23 18:34:28 +0100

    Changed some verification values

--- Texture/look_around.m
+++ Texture/look_around.m
@@ -1,92 +1,78 @@
 function [ cordX, cordY ] = look_around(x, y, im, spot)
 	cordX = x;
 	cordY = y;
-disp(1)	
+
 	color = im(y, x, :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(2)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 	
 	color = im((y + 1), x, :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(3)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	color = im((y + 1), (x + 1), :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(4)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	color = im((y - 1), (x + 1), :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(5)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	color = im((y - 1), (x - 1), :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-		disp(color)
-disp(6)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	color = im((y + 1), (x - 1), :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(7)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	color = im((y - 1), x, :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(8)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	color = im(y, (x - 1), :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(9)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	color = im(y, (x + 1), :);
-	if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
-disp(q)	
+	if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 		return;
 	end
 
 	switch spot
 		case 'l'
-			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 				cordX = (x + 2);
 				cordY = y;
-disp(w)	
 				return;
 			end
 			[cordX, cordY] = look_around((x + 2), y, im, spot);
 		case 'r'
-			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 				cordX = (x - 2);
 				cordY = y;
-disp(e)	
 				return;
 			end
 			[cordX, cordY] = look_around((x - 2), y, im, spot);
 		case 'u'
-			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 				cordX = x;
 				cordY = (y + 2);
-disp(r)	
 				return;
 			end
 			[cordX, cordY] = look_around(x, (y + 2), im, spot);
 		case 'd'
-			if color(1) ~= 0 || color(2) ~= 0 || color(3) ~= 0
+			if color(1) >= 6 || color(2) >= 6 || color(3) >= 6
 				cordX = x;
 				cordY = (y - 2);
-disp(t)	
 				return;
 			end
 			[cordX, cordY] = look_around(x, (y - 2), im, spot);
