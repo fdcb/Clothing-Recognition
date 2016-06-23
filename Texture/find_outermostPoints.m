baafd221b01401baffa9aeef5fb2eafce95920cb
fdcb <fdcb@live.com.pt>
2016-06-23 19:51:38 +0100

    find_outermost back to only finding 1 point for each

--- Texture/find_outermostPoints.m
+++ Texture/find_outermostPoints.m
@@ -1,5 +1,5 @@
 function [ points ] = find_outermostPoints(x, y)
-	points = cell(8, 4);
+	points = cell(4, 4);
 
 	[maxX, iMaxX] = max(x);
 	[minX, iMinX] = min(x);
@@ -10,24 +10,5 @@ function [ points ] = find_outermostPoints(x, y)
 	points(2, :) = {minX, y(iMinX), 'l', iMinX};
 	points(3, :) = {x(iMaxY), maxY, 'd', iMaxY};
 	points(4, :) = {x(iMinY), minY, 'u', iMinY};
-
-	x(iMaxX) = [];
-	x(iMinX) = [];
-	y(iMaxX) = [];
-	y(iminX) = [];
-	y(iMaxY) = [];
-	y(iMinY) = [];
-	x(iMaxY) = [];
-	x(iMinY) = [];
-
-	[maxX, iMaxX] = max(x);
-	[minX, iMinX] = min(x);
-	[minY, iMinY] = min(y);
-	[maxY, iMaxY] = max(y);
-
-	points(5, :) = {maxX, y(iMaxX), 'r', iMaxX};
-	points(6, :) = {minX, y(iMinX), 'l', iMinX};
-	points(7, :) = {x(iMaxY), maxY, 'd', iMaxY};
-	points(8, :) = {x(iMinY), minY, 'u', iMinY};
 
 end
