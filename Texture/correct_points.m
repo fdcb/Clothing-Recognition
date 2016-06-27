46434c099f35a08b23a8cb65da6621dc2741e902
fdcb <fdcb@live.com.pt>
2016-06-27 19:45:40 +0100

    Changed verifications

--- Texture/correct_points.m
+++ Texture/correct_points.m
@@ -10,28 +10,31 @@ function [ x, y ] = correct_points(x, y, im, spot, oldX, oldY, local)
 
 	if strcmp(spot, 'd') || strcmp(spot, 'u')
 		if strcmp(local, 'br') || strcmp(local, 'tr')
-			[x, y] = fix_points((oldX - 3), oldY, (oldX - 4), oldY);
+			if (oldX - 3) > 0
+				[x, y] = fix_points((oldX - 3), oldY, (oldX - 4), oldY);
+			end
 		else %bl or tl
-			[x, y] = fix_points((oldX + 3), oldY, (oldX + 4), oldY);
+			if (oldX + 3) < w
+				[x, y] = fix_points((oldX + 3), oldY, (oldX + 4), oldY);
+			end
 		end
 	else % l or r
 		if strcmp(local, 'br') || strcmp(local, 'bl')
-			[x, y] = fix_points(oldX, (oldY - 3), oldX, (oldY - 4));
+			if (oldY - 3) > 0
+				[x, y] = fix_points(oldX, (oldY - 3), oldX, (oldY - 4));
+			end
 		else %tl or tr
-			[x, y] = fix_points(oldX, (oldY + 3), oldX, (oldY + 4));
+			if (oldY + 3) < h
+				[x, y] = fix_points(oldX, (oldY + 3), oldX, (oldY + 4));
+			end
 		end
 	end
 
 	function [ x, y ] = fix_points(cordX, cordY, cordX1, cordY1)
-		if cordX > w || cordX < 0 || cordY > h || cordY < 0
-			return;
-		end
 		color = im(cordY, cordX, :);
 		if color(1) >= 15 || color(2) >= 15 || color(3) >= 15
 			x = cordX;
 			y = cordY;
-			% [x , y] = correct_points(cordX1, cordY1, im, spot, ...
-			% 		oldX, oldY, local);
 			return;
 		end
 		[x, y] = look_around(cordX, cordY, im, spot);
