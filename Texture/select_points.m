71273307ea258750ebf8e9f8f4d945cf889b7622
fdcb <fdcb@live.com.pt>
2016-06-29 15:37:17 +0100

    Removed unecessary lines.

--- Texture/select_points.m
+++ Texture/select_points.m
@@ -3,16 +3,11 @@ function [ x, y ] = select_points(x, y, im)
 	tempX = x;
 	tempY = y;
 	for i = 1 : length(points)
-	%i=1;
 		cordX = round(cell2float(points(i, 1)));
 		cordY = round(cell2float(points(i, 2)));
 		spot = char(points(i, 3));
 		index = cell2mat(points(i, 4));
-plot(cordX, cordY, 'rs');
 		[newX, newY] = look_around(cordX, cordY, im, spot);
-		local = locate_points(newX, newY, im);
-		[newX, newY] = correct_points(newX, newY, im, spot, ...
-				cordX, cordY, local);
 		if newX ~= cordX || newY ~= cordY
 			x(index) = newX;
 			y(index) = newY;
