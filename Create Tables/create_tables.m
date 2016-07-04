7e4c93859924ffdb4c98ea2a4828292bf17efbb9
fdcb <fdcb@live.com.pt>
2016-07-04 20:17:17 +0100

    stuff was done

--- Create Tables/create_tables.m
+++ Create Tables/create_tables.m
@@ -1,4 +1,4 @@
-function create_tables(color, texture)
+function [color_texture] =  create_tables(color, texture)
 
 	color = importdata(color, ' ');
 	texture = importdata(texture, ' ');
@@ -14,26 +14,38 @@ function create_tables(color, texture)
 	color_perc = color(: , 2 : 4 : end);
 	for i = 1 : x
 		temp = color_perc(i, :);
-		temp(isnan(temp)) = [];
-		if temp >= color_avg
+		temp(isnan(temp)) = 0;
+        temp =  temp(temp ~= 0);
+		if length(temp) > color_avg
 			temp = sort(temp, 'descend');
 			temp = temp(1 : color_avg);
 			row = [];
-			for j = 1 : 4 : y
-				if isnan(color_perc(i, j))
+			for j = 2 : 4 : y
+				if isnan(color(i, j))
 					break;
-				elseif ~isempty(find(temp == color_perc(i, j))
-					row_temp = color_perc(i, j : (j + color_avg - 1));
+				elseif ~isempty(find(temp == color(i, j), 1))
+					row_temp = color(i, j : (j + 3));
 					row = cat(2, row, row_temp);
-				end
+                end
+                if length(row) == (4 * color_avg)
+                    break;
+                end
 			end
-		elseif
-			row = color_perc(1, 2 : end);
-			row(isnan(row)) = [];
+        else
+			row = color(i, 2 : end);
+			row(isnan(row)) = 0;
+            row = row(row ~= 0);
 			[tx, ty] = size(row);
-			temp = zeros([1, (4 * color_avg - tx);
-			row = cat(2, row, temp);
-		end
+            if ty ~= (4 * color_avg)
+                temp = zeros(1, ((4 * color_avg) - ty));
+                row = cat(2, row, temp);
+            end
+        end
+        i
 		new_color(i, :) = row;
-	end
+        
+    end
+    
+    texture(isnan(texture)) = 0;
+    color_texture = cat(2, new_color, texture);
 end
